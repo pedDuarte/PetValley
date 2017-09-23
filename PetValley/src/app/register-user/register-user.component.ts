52600629cd97b20599f514748c4cf3072be1ed08
Diogo Lourenço Pereira <diogo-lp@hotmail.com.br>
2017-09-23 06:22:12 -0300

    Atualização das telas de cadastro com a inserção de alguns parametros do angular.

--- PetValley/src/app/register-user/register-user.component.ts
+++ PetValley/src/app/register-user/register-user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import {User} from '../model/user.model';
+import {RegisterUserService} from './register-user.service';
 
 @Component({
   selector: 'pet-register-user',
@@ -7,9 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+
+  private user: User = new User();
+  returnUrl: string;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private registerUserService: RegisterUserService) {}
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.registerUserService.registerUser(this.user);
   }
 
 }
--- PetValley/src/app/register-user/register-user.service.ts
+++ PetValley/src/app/register-user/register-user.service.ts
@@ -12,7 +12,8 @@ export class RegisterUserService {
 
     }
 
-    regiterUser() {
+    registerUser(user: User) {
+        
 
     }
 
