23cef3ade00f9987216429da8bc84980aee6d4ab
Diogo <diogo-lp@hotmail.com.br>
2017-11-09 19:11:35 -0200

    Implementação do binding de dados no modal de pet.

--- src/app/edit-pet/edit-pet.component.ts
+++ src/app/edit-pet/edit-pet.component.ts
@@ -1,3 +1,4 @@
+import { Pet } from './../model/pet.model';
 import { Router } from '@angular/router';
 import { PetServices } from './../services/petServices.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -11,7 +12,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class EditPetComponent implements OnInit {
   @Input()
-  private pet;
+
+  private pet
 
   private petEditForm: FormGroup;
 
@@ -28,10 +30,27 @@ export class EditPetComponent implements OnInit {
         size: this.formBuilder.control('', [Validators.required]),
         coat: this.formBuilder.control('', [Validators.required]),
         age: this.formBuilder.control('', [Validators.required]),
+        neutered: this.formBuilder.control('',[Validators.required]),
+        vermifuges: this.formBuilder.control('',[Validators.required]),
+        name: this.formBuilder.control('', [Validators.required]),
+        description: this.formBuilder.control('',[Validators.required]),
         species: this.formBuilder.control('', [Validators.required]),
         sex: this.formBuilder.control('', [Validators.required])
       });
     }
   }
 
-  }
+  loadPet() {
+  this.petEditForm.patchValue({
+    size: this.pet.size,
+    coat: this.pet.coat,
+    age:  this.pet.age,
+    neutered: this.pet.neutered,
+    vermifuges: this.pet.vermifuges,
+    name: this.pet.name,
+    description:  this.pet.description,
+    species: this.pet.species,
+    sex: this.pet.sex})
+ }
+
+}
