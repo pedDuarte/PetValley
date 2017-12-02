28bcf627310a01c3aa56c2ebcca9f150174949da
Pedro Duarte <pedrolu.duarte@gmail.com>
2017-12-02 09:05:36 -0200

    Public para visualização Heroku  - Login Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private loginService: LoginService,
+  constructor(public loginService: LoginService,
               private router: Router) { }
 
   ngOnInit() {
